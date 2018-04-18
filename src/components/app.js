5fb60a5db8f2509480cd19cad54cae743acdf486
Victor <victorlaucas@gmail.com>
2018-04-18 14:07:45 -0600

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Course Library</h1>
+      </div>
+    );
+  }
+}
