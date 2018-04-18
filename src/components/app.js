791a9014a1c6cdb062e9c4e32e91b15049ae54af
Victor <victorlaucas@gmail.com>
2018-04-18 14:52:10 -0600

    Created courseLibrary component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CourseLibrary from './courseLibrary';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Course Library</h1>
+        <CourseLibrary/>
       </div>
     );
   }
