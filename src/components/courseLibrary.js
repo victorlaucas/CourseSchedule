23fb20901af3a7182f68762d02366a6fbe2f2d5d
Victor <victorlaucas@gmail.com>
2018-04-18 15:41:49 -0600

    Introduced redux into app. Created courses reducer

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CourseLibrary extends Component {
-
 
     constructor(props) {
         super(props)
@@ -11,7 +11,7 @@ class CourseLibrary extends Component {
 
     renderCourse(course) {
         return (
-        <li className="course">
+        <li key={course.title} className="course">
             <div className="course__info">
                 <div className="course__title-container">
                     <div className="course__title">{course.title}</div>
@@ -26,14 +26,20 @@ class CourseLibrary extends Component {
     }
 
     render() {
+        
         return ( 
             <ul>
-                {this.renderCourse({'title': 'Up and Running with Redis', 'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.'})}
-                {this.renderCourse({'title': 'UX for Developers', 'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.'})}
+                {this.props.courses.map(this.renderCourse)}
             </ul>
         )
     }
 
 }
 
-export default CourseLibrary; 
+
+function mapStateToProps(state) {
+    console.log(`state courses are : ${JSON.stringify(state.courses)}`)
+    return { courses: state.courses }
+}
+
+export default connect(mapStateToProps)(CourseLibrary);
