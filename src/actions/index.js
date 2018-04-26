b130232770f82a043af6b6af6245287a5accce7d
Victor <victorlaucas@gmail.com>
2018-04-26 16:17:09 -0600

    Implemented remove course action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,28 @@
-import { FETCH_COURSES } from './types';
+import { 
+  FETCH_COURSES,
+  REMOVE_COURSE
+} from './types';
 
 export function fetchCourses() {
-    return {
-        type: FETCH_COURSES,
-        payload: [
-            {
-                'title': 'Up and Running with Redis',
-                'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.'
-            },
-            {
-                'title': 'UX for Developers',
-                'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.'
-            }
-        ]
-    }
+  return {
+      type: FETCH_COURSES,
+      payload: [
+          {
+              'title': 'Up and Running with Redis',
+              'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.'
+          },
+          {
+              'title': 'UX for Developers',
+              'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.'
+          }
+      ]
+  }
+}
+
+
+export function removeCourse(course) {
+  return {
+      type: REMOVE_COURSE,
+      payload: course
+  }
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,2 @@
 export const FETCH_COURSES = 'fetch_courses';
+export const REMOVE_COURSE = 'remove_course';
