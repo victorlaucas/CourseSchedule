8a5e7d35a876a128e3ff9ccd10591f3659f4c724
Victor <victorlaucas@gmail.com>
2018-04-26 16:42:47 -0600

    Added remove course reducer and remove course action

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,11 +9,13 @@ export function fetchCourses() {
       payload: [
           {
               'title': 'Up and Running with Redis',
-              'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.'
+              'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.',
+              'enrolled': true
           },
           {
               'title': 'UX for Developers',
-              'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.'
+              'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.',
+              'enrolled': true
           }
       ]
   }
