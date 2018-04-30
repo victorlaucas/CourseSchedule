3d8c0a8ca6de6a97fa23e32f86a714eeb74f2532
Victor <victorlaucas@gmail.com>
2018-04-30 11:02:45 -0600

    Implemented add and remove button to course library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import { 
   FETCH_COURSES,
-  REMOVE_COURSE
+  REMOVE_COURSE,
+  ADD_COURSE
 } from './types';
 
 export function fetchCourses() {
@@ -10,12 +11,12 @@ export function fetchCourses() {
           {
               'title': 'Up and Running with Redis',
               'description': 'In this course you\'ll learn how to work with the efficient Redis database to manage key value relationships.',
-              'enrolled': true
+              'enrolled': false
           },
           {
               'title': 'UX for Developers',
               'description': 'This User Experience (UX) course examines how to develop a system for approaching application development and enhancing the experience for users.',
-              'enrolled': true
+              'enrolled': false
           }
       ]
   }
@@ -25,6 +26,13 @@ export function fetchCourses() {
 export function removeCourse(course) {
   return {
       type: REMOVE_COURSE,
+      payload: course
+  }
+}
+
+export function addCourse(course) {
+  return {
+      type: ADD_COURSE,
       payload: course
   }
 }
