853d637c0eabe8a547c55089e485e46c1c594a74
Victor <victorlaucas@gmail.com>
2018-04-30 13:26:11 -0600

    Added hover styles

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect, dispatch } from 'react-redux';
-import { fetchCourses, addCourse, removeCourse, toggleDescription } from '../actions';
-import AnimateHeight from 'react-animate-height';
+import { fetchCourses, addCourse, removeCourse, toggleDescription } from '../actions'
+import AnimateHeight from 'react-animate-height'
 
 class CourseLibrary extends Component {
 
@@ -17,7 +17,7 @@ class CourseLibrary extends Component {
 
     renderCourse(course) {
         return (
-        <li key={course.title} className="course">
+        <li key={course.title} className={`course ${course.open ? 'course__selected' : ''}`}>
             <div className="course__info">
                 <div className="course__title-container">
                     <div className="course__title">{course.title}</div>
