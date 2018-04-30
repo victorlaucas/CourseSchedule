d6c1a7edf2dfe2041ab40577e49bf6db75dbd6a1
Victor <victorlaucas@gmail.com>
2018-04-30 16:29:21 -0600

    Added remove course from schedule style

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -18,7 +18,7 @@ class Schedule extends Component {
         return (
             <div key={this.state.enrolled.indexOf(course)} className={`slot ${course.enrolled ? 'slot__course' : 'slot__empty'}`}>
                 <div>{course.enrolled ? course.title : 'Empty Slot'}</div>
-                <a className={`action slot__remove`} onClick={() => this.props.removeCourse(course)}>remove course</a>
+                <a className={`action slot__remove`} onClick={() => this.props.removeCourse(course)}></a>
             </div>
         )
     }
